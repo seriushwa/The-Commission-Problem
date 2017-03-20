3b85bddf911c99fd23b6451915619ed9e9c4b1ef
seriushwa <seriushwa.cs00@gmail.com>
2017-03-20 17:09:07 +0800

    Add feature code without test

--- commission.cpp
+++ commission.cpp
@@ -3,11 +3,19 @@
 
 int sellResult(int lock,int stock,int barrel)
 {
-	return 0;
+	if(lock<1 && stock<1 && barrel<1)return 0;
+	if(lock>70 && stock>80 && barrel>90)return 0;
+	
+	return lock*45+stock*30+barrel*25;
 }
 int salary(int result)
 {
-	return 0;
+	int pay = 0;
+	if(result>1800)pay=(result-1800)*(20/100)+100+120;
+	else if(result>1000)pay=(result-1000)*(15/100)+100;
+	else pay=result*(10/100);
+
+	return pay;
 }
 
 TEST(sellResult,Boundary)
